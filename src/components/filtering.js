d776c2442b244d52908de051e67576f43e1bb48b
Dima Kotov <DimsiKotov@yandex.ru>
2025-08-10 12:37:26 +0300

    тест

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,38 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+ const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes).forEach((elementName) => {
+        elements[elementName].innerHTML = ''; // Очистка существующих опций перед добавлением новых
+        elements[elementName].append(
+            ...Object.values(indexes[elementName])
+                .map(name => `<option value="${name}">${name}</option>`).join('')
+        );
+    });
+    
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if (action.type === 'clear') {
+            const buttonElement = document.querySelector(`button[name='${action.name}']`);
+            
+            if (!buttonElement) return data;
+        
+            const parentElement = buttonElement.parentNode;
+            const fieldName = buttonElement.getAttribute('data-field');
+        
+            const inputField = parentElement.querySelector(`input[data-field='${fieldName}']`);
+        
+            if (inputField) {
+                inputField.value = '';
+                state[fieldName] = '';
+            }
+        }
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
--- src/components/searching.js
+++ src/components/searching.js
@@ -2,10 +2,23 @@ import {rules, createComparison} from "../lib/compare.js";
 
 
 export function initSearching(searchField) {
+    const comparison = createComparison(
+        [
+            rules.skipEmptyTargetValues,
+            rules.searchMultipleFields(searchField, ["date", "customer", "seller"], false)
+        ]
+    );
     // @todo: #5.1 — настроить компаратор
 
     return (data, state, action) => {
+        if (!state.filters || !state.filters.length) {
+            return data;
+        }
         // @todo: #5.2 — применить компаратор
-        return data;
-    }
+        let filteredData = data.filter((item) =>
+            comparison(item, state.filters.find(f => f.field === searchField))
+        );
+        
+        return filteredData;
+    };
 }
